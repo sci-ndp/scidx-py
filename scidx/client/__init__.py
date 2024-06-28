20c6424e3471182d60a038bc7369786500b963ea
Andreufb <andreufb97@gmail.com>
2024-06-28 13:22:27 -0600

    fix: Import kafka functionalitites

--- scidx/client/__init__.py
+++ scidx/client/__init__.py
@@ -4,12 +4,15 @@ from .search_resource import search_resource
 from .search_organization import search_organization
 from .register_url import register_url
 from .register_s3 import register_s3
+from .register_kafka import register_kafka
+from .search_kafka import search_kafka
 
 
 # Add the methods to sciDXClient
 sciDXClient.register_organization = register_organization
+sciDXClient.search_kafka = search_kafka
 sciDXClient.search_organization = search_organization
 sciDXClient.search_resource = search_resource
 sciDXClient.register_url = register_url
+sciDXClient.register_kafka = register_kafka
 sciDXClient.register_s3 = register_s3
-
